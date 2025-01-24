64a082739e1afae12dbb50d802b325c9c2889289
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-01-24 19:16:31 +0530

    backend setup

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,10 @@
+//Route for login user
+const loginUser = async (req, res) => {};
+//Route for Register user
+const registerUser = async (req, res) => {
+  res.json({ message: "Admin Login Route" });
+};
+
+// Route for admin login
+const adminLogin = async (req, res) => {};
+export { loginUser, registerUser, adminLogin };

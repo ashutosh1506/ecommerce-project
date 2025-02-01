752d64ab797eb554d8907997e77ad73bea399b3e
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-02-01 18:45:00 +0530

    orders functionality

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,63 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+// Placing order using COD
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, {
+      cartData: {},
+    });
+
+    res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Placing order using stripe
+const placeOrderStripe = async (req, res) => {};
+
+// Placing order using Razorpay
+const placeOrderRazorPay = async (req, res) => {};
+
+// All Orders data for admin Panel
+const allOrders = async (req, res) => {};
+
+// User Orders data for Frontend
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await orderModel.find({ userId });
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Update Order Status fron admin panel
+const updateStatus = async (req, res) => {};
+
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorPay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};

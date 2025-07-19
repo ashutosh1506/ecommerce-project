b2ca7cfd0aa4d9d3f9d1b8381871135873cc9fe5
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-07-19 20:01:53 +0530

    Data Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div>
           <img src={assets.logo} alt="" className="mb-5 w-32" />
           <p className="w-full text-justify md:w-2/3 text-gray-600">
-            At FOREVER, we create more than just clothes; we design timeless
+            At Stello, we create more than just clothes; we design timeless
             pieces that last. Our collections are built on a foundation of
             quality craftsmanship and enduring style, made to be cherished in
             your wardrobe for years to come.
@@ -29,14 +29,14 @@ const Footer = () => {
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
             <li>+91-222-333-555-0</li>
-            <li>contact@foreveryou.com</li>
+            <li>contact@stello.com</li>
           </ul>
         </div>
       </div>
       <div>
         <hr />
         <p className="py-5 text-sm text-center">
-          Copyright 2025@ forever.com - All Rights Reserved.
+          Copyright 2025@ Stello .com - All Rights Reserved.
         </p>
       </div>
     </div>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
             Tel: (415) 555-0132 <br /> Email: admin@gmail.com
           </p>
           <p className="font-semibold text-xl text-gray-600">
-            Carrers at Forever
+            Carrers at Stello
           </p>
           <p className="text-gray-500">
             Learn more about our teams and job opening.
